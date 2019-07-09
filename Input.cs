dc8e61859d2fe7f30987c79ef0e6ebd9e12acf99
Mike Dumlao <mikhaild908@gmail.com>
2019-07-09 16:40:02 -0700

    Create Input.cs

---
+++ Input.cs
@@ -0,0 +1,10 @@
+namespace Test_Review
+{
+    public class Input
+    {
+        public string requestmethod { get; set; }
+        public string requesturl { get; set; }
+        public string requestbody { get; set; }
+        public string requestprotocol { get; set; }
+    }
+}
