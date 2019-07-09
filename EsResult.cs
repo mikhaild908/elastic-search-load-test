b5953b245c428fdf7348e4dbedd03fbeb98f42c2
Mike Dumlao <mikhaild908@gmail.com>
2019-07-09 16:40:17 -0700

    Create EsResult.cs

---
+++ EsResult.cs
@@ -0,0 +1,10 @@
+using System;
+namespace Test_Review
+{
+    public class EsResult
+    {
+        public bool success { get; set; }
+        public bool statusCode { get; set; }
+        public EsData data { get; set; }
+    }
+}
